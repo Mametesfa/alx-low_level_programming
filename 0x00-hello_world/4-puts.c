1bf401710875c74fbef1af1077c1fae30300328d
Mametesfa <aberatesfahiwot@gmail.com>
2022-12-08 01:37:09 -0800

    put in to c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- *
- *  * Description: main - Prints out a string to standard out.
- *
- *   * Return: 0 if success.
- *
- *    */
+ * Description: main - Prints out a string to standard out.
+ * Return: 0 if success.
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
