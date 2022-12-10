3323d69480180401042484be17d790e6d7fb9201
Mametesfa <aberatesfahiwot@gmail.com>
2022-12-09 18:44:44 -0800

    combination three

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
- * Return: Always 0 (Success)
+ * main - program that prints all possible different combinations ofthree digits
+ * Numbers must be separated by ,followed by a space
+ * The two digits must be different
+ * 012, 120, 102, 021, 201, 210 are considered the same combination
+ * Return: 0
  */
 
 int main(void)
 {
-	int n1, n2;
-	for (n1 = 0; n1 < 100; n1++)
+	int n1 = 48;
+	int n2, n3;
+	int com = 44;
+
+	while (n1 <= 57)
 	{
-		for (n2 = 0; n2 < 100; n2++)
+		n2 = n1 + 1;
+		while (n2 <= 57)
 		{
-			if (n1 < n2)
+			n3 = n2 + 1;
+			while (n3 <= 57)
 			{
-				putchar((n1 / 10) + 48);
-				putchar((n1 % 10) + 48);
-				putchar(' ');
-				putchar((n2 / 10) + 48);
-				putchar((n2 % 10) + 48);
-				if (n1 != 98 || n2 != 99)
+				putchar(n1);
+				putchar(n2);
+				putchar(n3);
+				if (n1 != 55 || n2 != 56 || n3 != 57)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(com);
+					putchar(32);
 				}
+				n3 += 1;
 			}
+			n2 += 1;
 		}
+		n1 += 1;
 	}
 	putchar('\n');
 	return (0);
