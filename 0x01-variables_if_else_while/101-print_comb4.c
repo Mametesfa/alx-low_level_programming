3fa2857a5b757ea6ad9a20f09b7c83be870b9823
Mametesfa <aberatesfahiwot@gmail.com>
2022-12-09 18:48:16 -0800

    combination three

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all possible different combinations ofthree digits
+ * main - program that prints all possible different combinations of
+ * three digits
  * Numbers must be separated by ,followed by a space
  * The two digits must be different
  * 012, 120, 102, 021, 201, 210 are considered the same combination
