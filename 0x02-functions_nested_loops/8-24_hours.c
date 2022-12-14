ad239d5e277a4b7184022eb21aa63d85bda23292
TESFAHIWOT ABERA AYELE <117796896+Mametesfa@users.noreply.github.com>
2022-12-14 06:55:27 +0300

    Minute of a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,15 +1,15 @@
-#include "main.h" 
+#include "main.h"
 /**
  * jack_bauer -  function that prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59.
  * Return: nothing.
  */
-
- void jack_bauer(void)
+void jack_bauer(void)
 {
 	int x = 0;
 	int j = 0;
-	while (x <= 23)
+	
+	 while (x <= 23)
 	{
 		j = 0;
 		while (j <= 59)
