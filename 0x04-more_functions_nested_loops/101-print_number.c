99a0f24a011de8ae16fe07b4fabdee200dcfaf7d
Mametesfa <aberatesfahiwot@gmail.com>
2022-12-16 01:27:42 -0800

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,9 +6,9 @@
  */
 void print_number(int n)
 {
-	long m; /* power of 10 */
-	int c; /* boolean check */
-	long num; /* convert int to long */
+	int m; /* power of 10 */
+	float c; /* boolean check */
+	int num;
 
 	num = n;
 	/* negatives */
