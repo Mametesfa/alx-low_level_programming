bd614db373136ba2659c3c5cbbb395d35787bb19
Mametesfa <aberatesfahiwot@gmail.com>
2023-01-20 01:13:44 +0000

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - this program prints all the arguments
+(* a blank line
+*@argc: this is the quantity of parameters
+*@argv: the parameter to put in the program.
+* Description: this program prints all the arguments)?
+* Return: return 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
